04be22855c4d051b1f61e9a026c1f08c6c565654
Atisa <40288907+atisawd@users.noreply.github.com>
2021-12-13 10:27:02 +0530

    gtag

---
+++ _next/static/90G5f-sA3uPVYFiYCNVfI/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/699-44ebecd1ef1ce0cd.js","static/css/3bcc7d27361e9aaa.css","static/chunks/pages/index-39f04ccf13350a06.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
