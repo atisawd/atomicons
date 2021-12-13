b1ffaa5deb123c70c31925157829085639675fe9
Atisa <40288907+atisawd@users.noreply.github.com>
2021-12-13 09:42:44 +0530

    font

--- _next/static/mMyatQmyZSyVPQ3vme0IY/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/699-05057066bb013f5a.js","static/css/a697e57e50aba790.css","static/chunks/pages/index-008506bd03efa596.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
